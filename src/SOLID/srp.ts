f5ca5786d8b3a399e48e73062bd231235cc901a1
kamlesh70 <kamalmehra3571@gmail.com>
2025-09-04 09:23:57 +0530

    feat: add factory-pattern

---
+++ src/SOLID/srp.ts
@@ -0,0 +1,7 @@
+/* 
+  Single Responsibility Principle
+  One class should do job or should have one purpose, If one class is doing more than one job It will be tightly coupled and doing change in one can effect other as well. 
+
+  So we always try to de-coupled the class so that changes are not effect other class or object or behaviors.
+
+*/
--- src/creational-pattern/2.factory-pattern/index.ts
+++ src/creational-pattern/2.factory-pattern/index.ts
@@ -1,7 +1,58 @@
-export class FactoryPattern {
-  readonly name: String | undefined;
+interface PaymentGateway {
+  makePayment(client: any, amount: number): any;
+  refund(client: any, order: any): any;
+  getPaymentDetails(paymentId: number | string): any;
+}
 
-  constructor() { };
+class StripePaymentGateway implements PaymentGateway {
+  public makePayment() {
+    console.log(StripePaymentGateway.name, "make payment");
+  }
 
-  setName(): void {}
+  public refund() {}
+
+  public getPaymentDetails() {}
+}
+
+class PaypalPaymentGateway implements PaymentGateway {
+  public makePayment() {
+    console.log(PaypalPaymentGateway.name, "make payment");
+  }
+
+  public refund() {}
+
+  public getPaymentDetails() {}
+}
+
+class PaymentGatewayFactory {
+  public static getInstance(gatewayName: string): PaymentGateway | undefined {
+    switch (gatewayName) {
+      case StripePaymentGateway.name: {
+        return new StripePaymentGateway();
+      }
+
+      case PaypalPaymentGateway.name: {
+        return new PaypalPaymentGateway();
+      }
+
+      default: {
+        console.log("getting error while creating paymentGateway instance");
+      }
+    }
+  }
+}
+
+export function clientSide() {
+  try {
+    const paymentGateway = PaymentGatewayFactory.getInstance(
+      PaypalPaymentGateway.name
+    );
+    if (!paymentGateway) {
+      throw new Error("payment gateway instance not found");
+    }
+
+    paymentGateway.makePayment({ name: "client_details" }, 12.1);
+  } catch (error) {
+    console.log("getting from payment gate client side code", error);
+  }
 }
